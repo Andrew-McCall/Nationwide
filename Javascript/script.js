871812a4e2b6140e522f98bc8acad11fcf21eec4
Andrew-McCall <amccall@qa.com>
2023-06-30 12:21:41 +0100

    Conditons and Loops

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,89 +1,143 @@
-// let [VariableName] = [Data]
-// Name can anything withoutspaces
-// Cant start with special character or numbers
-const string2 = "Text";
-let number2 = 0.54;
+"use strict"; // Limits javascripts loose typing
 
-let unassigned; // undefined - No data has been given
-console.log(unassigned)
+// Comments get ignored
+// Code is just instructions
 
-let example = null // how to assgin the lack of data. Null is data, nothing
-
-let unqiue = Symbol(2); // Used for ids. 
-console.log(unqiue)
-
-
-
-// == & ===
-console.log(example === unassigned)
-
-number2 = 0
-console.log("" + number2) // 0
-
-number2 = null
-console.log("" + number2) // "null" - becomes string
-
-number2 = 0
-console.log("" + number2) // 0
-
-const thisCannotBeChanged = 0;
-// Cant change!  thisCannotBeChanged = 50;
-
-let array = [1,2,3,4,5,6]
-console.log(array)
-console.log("first element is : " + array[0])
-console.log("length is: "+ array.length)
-
-
-let object = {}
-let carObject = { model:"Panda", brand:"Fiat", year:2002, driver:object, list:array }
-
-carObject.driver.name = "Andrew"
-
-console.log(typeof carObject)
-
-console.log(carObject)
-console.log(carObject.model)
-carObject.model = "500"
-console.log(carObject.model)
-
-// .methodName() -  method
-//            {} -  object
-//            [] -  array
-
-function test() {
-    let number = 0
-    console.log(number)
+// ` backtick, for a string template. 
+// Mix text and code easily
+//console.log(`${10} bottles on the wall, if one falls then there'll be ${9} bottles left.`)
+let loopCounter = 10
+while (loopCounter > 0) {
+    console.log(loopCounter + " bottles on the wall, if one falls then there'll be "+ (loopCounter-1) + " bottles left.")
+    loopCounter = loopCounter - 1
 }
 
-let number = 9;
-test()              // Prints 0
-console.log(number) // Then prints 9
-
-var moo = "Hello"
-let mooo = "Hello"
-{
-var moo = "World"
-let mooo = "World"
-}
-
-console.log(moo)
-console.log(mooo)
-
-// Backticks `` with ${} inside - string literal
-let variable = `i can mix text ${"And Code" + 5*60} and more text ${number2} ${string2} ${test()}`
-console.log(variable)
-
-/*
-(6) [1, 2, 3, 4, 5, 6]
-script.js:10 first element is : 1
-script.js:11 length is: 6
-script.js:19 object
-script.js:21 {model: 'Panda', brand: 'Fiat', year: 2002, driver: {…}, list: Array(6)}
-script.js:22 Panda
-script.js:24 500
-script.js:32 0
-script.js:37 9
-script.js:46 World
-script.js:47 Hello
+/*          
+10 bottles on the wall, if one falls then there'll be 9 bottles left.
+ 9 bottles on the wall, if one falls then there'll be 8 bottles left.
+ 8 bottles on the wall, if one falls then there'll be 7 bottles left.
+ 7 bottles on the wall, if one falls then there'll be 6 bottles left.
+ 6 bottles on the wall, if one falls then there'll be 5 bottles left.
+ 5 bottles on the wall, if one falls then there'll be 4 bottles left.
+ 4 bottles on the wall, if one falls then there'll be 3 bottles left.
+ 3 bottles on the wall, if one falls then there'll be 2 bottles left.
+ 2 bottles on the wall, if one falls then there'll be 1 bottles left.
+ 1 bottles on the wall, if one falls then there'll be 0 bottles left.
 */
+
+console.log("")
+
+// Conditions
+console.log("Are True")
+
+console.log(0 != 1)   // Not Equal
+console.log(0 !== 1)  // Not Equal Strict
+console.log(0 == 0)   // Equal
+console.log(0 === 0)  // Equal Strict
+console.log(1 > 0)    // Greater Than
+console.log(0 < 1)    // Lesser Than
+console.log(2 >= 2)   // Greater Than OR Equal
+console.log(2 <= 2)   // Lesser Than OR Equal
+
+
+console.log("")
+console.log("Are complicated")
+console.log(0 == "0")  // True
+console.log(0 === "0") // False - Checks data type also
+
+let x = 5
+let y = 10
+console.log(y === x*2)          // True
+console.log(x < y && y === 10)  // True - && is AND
+console.log(x < y && y === 5)   // False
+console.log(x < y || y === 5)   // True - || is OR
+
+console.log((x < y || y === 5) && x === 5) // True - OR AND
+
+// BIDMAS
+// Brackets, Indicies, Divison, Multiplcation, Addition, Subtraction
+// (), **, /*, +-
+
+let z = 6+2/4
+console.log(z) // 6.5
+
+if (y === 10){
+    console.log("Y is equal to ten")
+}
+    
+if (x === 5){ // Code in brackets only executes if statement is met
+    console.log("X is equal to five")
+}else{
+    console.log("X is not five")
+}
+
+// Only checks the following ifs, IF the first are false
+if (false && 1 === 1 && true != false){
+    console.log("First IF")
+}else if (true){ 
+    console.log("ELSE IF")
+}else if (false){ 
+    console.log("ELSE ELSE IF")
+}else{
+    console.log("ELSE")
+}
+
+// Executes only WHILE statement is true
+while (y <= 1000){
+    y*= 2
+    console.log(y)
+}
+
+// Executes once, then checks/loops
+do {
+    console.log("Hello ")
+} while (false)
+
+// Will never happen, because its false
+while (false){
+    console.log("World!")
+}
+
+let i = 1 // 1
+i += 10   // i = i + 10 // 11
+i -= 5    // i = i - 5  // 6
+i *= 2    // i = i * 2  // 12
+i /= 4    // i = i / 4  // 3
+i++       // i = i + 1  // i+=1
+i--       // i = i - 1  // i-=1
+console.log(i)
+
+// 'delete' cannot be called on an variable in strict mode.
+//  delete i;
+
+// Type of loop, that manages its own counter variable
+//    variable        condition     loop-end
+for (let counter = 0; counter < 10; counter += 1){
+    console.log(counter)
+}
+
+console.log("ARRAY Milarcy")
+
+let array = []
+for (let oddNumbers = 1; oddNumbers < 10; oddNumbers += 2){
+    array.push(oddNumbers) // Added each odd number to the once empty array
+}
+
+
+for (let item of array){
+    console.log(item * 10)
+}
+
+for (let counter = 0; counter < array.length; counter++){
+    console.log(array[counter] * 10)
+}
+
+let counter = 0;
+while (counter < array.length){
+    console.log(array[counter] * 10)
+    counter++;
+}
+
+
+console.log(array)
+// console.log(array.map(item => item * 10))
