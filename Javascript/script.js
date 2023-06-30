740dcdeb893c9a0b92dfcc402c5420f6676d9cf2
Andrew-McCall <amccall@qa.com>
2023-06-30 15:24:39 +0100

    Pass by reference

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,102 +1,28 @@
+// Pass By Value - Primitive types
+let a = 10;
+let b = a;
 
-for (let A = 100; A <= 200; A++){
-    console.log(A);
-}
+a += 5;
 
-let a = 100;
-while (a <= 200){
-    if (a % 2 == 0){
-        console.log("-")
-    }else{
-        console.log("*")
-    }
-    a++;
-}
+console.log(a) // 15
+console.log(b) // 10
 
-for (let A = 100; A <= 200; A++){
-    if (A % 2 == 0){
-        console.log("-")
-    }else{
-        console.log("*")
-    }
-}
+// Pass By Reference - Objects, Arrays, Classes
+let c = {} 
+let d = c
+c["key"] = "Value"
 
-for (let x = 1; x<=10; x++){
-    
-    for (let y = 0; y < 10; y++){
-        console.log(x);
-    }
-}
+console.log(c) // {key: 'Value'}
+console.log(d) // {key: 'Value'}
 
-let day = "Sun"
-switch (day){
+d["key"] = "NO thanks!"
+console.log(c) // {key: 'NO thanks!'}
+console.log(d) // {key: 'NO thanks!'}
 
-    case "Mon":
-    case "Tue":
-    case "Wed":
-    case "Thu":
-    case "Fri":
-        console.log("Weekday!")
-        break;
 
-    case "Sat":
-        console.log("Saturday!")
-        break;
-
-    case "Sun":
-        console.log("Sunday!")
-        break;
-
-    default:
-        console.error("Invaild Day...")
-
-}
-
-const date = new Date()
-let dayAsNumber = date.getDay();
-switch (dayAsNumber){
-
-    case 1:
-    case 2:
-    case 3:
-    case 4:
-    case 5:
-        console.log("Weekday!")
-        break;
-
-    case 6:
-        console.log("Saturday!")
-        break;
-
-    case 0:
-        console.log("Sunday!")
-        break;
-
-    default:
-        console.error("Invaild Day...")
-
-}
-
-console.log(date)
-
-let strictA = true;
-let strictB = 1;
-console.log(strictA == strictB);  // T both are 1 (in value)
-console.log(strictA === strictB); // F one is bool, other is number
-
-console.log(strictA != strictB);  // F both are 1 so 
-console.log(strictA !== strictB); // T types are different
-
-let age = 5
-
-if (age < 18){
-    console.log("underage")
-}else if (age > 50){
-    console.log("over 50")
-}else{
-    console.log(`${age} is Between 65 and 18`)
-}
-
-console.log( (65 > age && age > 18)? "Between the range" : "Outside the range" )
-
-console.log( (age > 18)? ((age < 65)? `${age} is vaild`: "Overage" )  : "Underage" )
+let e = []
+let f = e
+e.push(1)
+f.push(-1)
+console.log(e) // {key: 'Value'}
+console.log(f) // {key: 'Value'}
