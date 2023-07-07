b4a4d954339320c6330de2e9ddcd868a223c132d
Andrew-McCall <amccall@qa.com>
2023-07-07 10:59:17 +0100

    Create and Read Cars using Mongoose

---
+++ Node/index.js
@@ -0,0 +1,93 @@
+const mongoose = require("mongoose")
+ 
+/*
+//                url for local mongo server
+console.log("Hello World")
+mongoose.connect("mongodb://127.0.0.1:27017")
+.then(() => {
+    console.log("Connected!")
+ 
+    // rest of code
+ 
+ 
+    mongoose.disconnect()
+    .then(() => console.log("Disconnect"))
+ 
+    console.log("End of mongoose connect, Alarm")
+}).catch(err => {
+    console.log("there was an error")
+})
+ 
+console.log("End of File")
+ 
+/// Console Log are:
+// Hello World
+// End of File
+// Connected!
+// End of mongoose connect, Alarm
+// Disconnect
+*/
+ 
+ 
+// .then, .catch are known as promises
+// https://qa-community.co.uk/~/_/learning/javascript/javascript--callbacks-and-promises
+ 
+ 
+mongoose.connect("mongodb://127.0.0.1:27017").then(() => {
+ 
+    console.log("Connected!")
+ 
+    carModel.create({
+         reg:"ABC124",
+         make:"Panda",
+         model:"Fiat",
+         colour:"Pink"
+    }).then(()=> {
+ 
+        carModel.find({}).then(res => {
+            for (let car of res){
+                console.log(car)
+            }
+ 
+            mongoose.disconnect()
+ 
+        })
+ 
+    })
+ 
+}).catch(err => {
+    console.log("There was an error")
+    console.log(err)
+})
+ 
+// schema is limitations on me creating
+const carSchema = new mongoose.Schema({
+    reg:String,
+    age:{type:Number, required:true},
+    make:String,
+    model:String,
+    colour:String
+})
+ 
+//                   collection name, blueprint of each doc,
+const carModel = mongoose.model("car", carSchema)
+// the collection as a js object that i can interact with easily
+ 
+// const schemaless = mongoose.model("people")
+// a model is a mongodb collection
+ 
+/// TASK
+// connect to the mongodb server using mongoose
+// create a schema
+// create a model from the schema
+// add a disconnect clause to the .then of the connect
+ 
+// create an object
+// read all objects
+ 
+/// EXT
+// install prompt-sync
+// (user inputs now)
+// make a path for creating an object
+// make an option to read all objects
+// make an option to update
